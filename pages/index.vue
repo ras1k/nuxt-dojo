21b82bf3fb89c3451a116e0ece8bda8e35df6f90
Rasik Abdullah <rasikabdullah@gmail.com>
2024-04-29 18:21:59 +0600

    update

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+<h2>Home</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque distinctio aliquid molestias aut? Necessitatibus, doloribus vero impedit iusto natus a facere expedita maiores sint? Ullam, corrupti quasi minus eius vitae fuga quos neque dignissimos veritatis, possimus, sint placeat exercitationem cupiditate voluptatibus dolores provident voluptatum voluptate impedit perspiciatis sunt architecto nulla.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
