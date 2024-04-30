2e95e8962d3c2974723f78202b6a3619cb3a0945
Rasik Abdullah <rasikabdullah@gmail.com>
2024-04-30 19:12:28 +0600

    update

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,9 +1,17 @@
 <script setup>
-const {id} = useRoute().params;
+const { id } = useRoute().params;
+const uri = 'https://fakestoreapi.com/products/' + id ;
+
+const { data: product } = await useFetch(uri, {key: id})
 </script>
 
 <template>
-  <div>lmao id details for {{ id }}</div>
+  <div>
+    <p>{{ product.id }}</p>
+    <img :src="product.image" alt="">
+    <p>{{ product.title }}</p>
+    <p>{{ product.price }}</p>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    lmao products
+    <h1>Products</h1>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="p in products">
+        <NuxtLink :to="`/products/${p.id}`">{{ p.title }}</NuxtLink>
+        <img :src='p.image' alt="">
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+const { data: products } = await useFetch('https://fakestoreapi.com/products')
 </script>
 
 <style lang="scss" scoped>
