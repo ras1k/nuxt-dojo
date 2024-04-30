f3914bd587df4930dbedb39348d5a0aee00bce9c
Rasik Abdullah <rasikabdullah@gmail.com>
2024-04-30 17:36:24 +0600

    update

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <header class="shadow-lg bg-white">
+      <nav class="container flex mx-auto p-4 justify-between">
+        <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
+        <ul class="flex gap-4 items-center">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="container px-[5%] py-[2%]">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .router-link-exact-active {
+    color: rgb(77, 165, 42);
+  }
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-about
+  <div class="container">
+    <h1 class="text-xl font-semibold">About Page</h1>
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero odit sed aspernatur voluptates esse doloribus necessitatibus id temporibus facere mollitia!
   </div>
 </template>
 
